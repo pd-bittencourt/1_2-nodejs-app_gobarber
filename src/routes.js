428ee81738e95a2ca9580422dc0dea8d4208f2bb
Pedro <pedrodmbc@gmail.com>
2019-04-28 17:59:39 -0300

    Register User

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.render('auth/signup')
-})
+const UserController = require('./app/controllers/UserController')
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
+
+routes.get('/', (req, res) => res.send('hello world'))
 
 module.exports = routes
