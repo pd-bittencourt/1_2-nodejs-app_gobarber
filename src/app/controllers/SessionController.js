f1fce83e55c32bc1c3bbf43c6cac1c0848e4e2ad
Pedro <pedrodmbc@gmail.com>
2019-04-28 22:25:47 -0300

    User authentication

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,25 @@
+const { User } = require('../models')
+
+class SessionController {
+  async create (req, res) {
+    return res.render('auth/signin')
+  }
+  async store (req, res) {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      console.log('user not found')
+      return res.redirect('/')
+    }
+
+    if (!(await user.checkPassword(password))) {
+      console.log('password incorrect')
+      return res.redirect('/')
+    }
+
+    return res.redirect('/app/dashboard')
+  }
+}
+module.exports = new SessionController()
--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,14 @@ const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
+// rotas signin
 routes.get('/', SessionController.create)
 routes.post('/signin', SessionController.store)
 
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
+// rota para dashboard
 routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
 module.exports = routes
